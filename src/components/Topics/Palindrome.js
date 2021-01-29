33e2a5eac24b42f84d4e301bb4c0be30262713bb
rachelseaborn <raseaborn@gmail.com>
2021-01-28 21:41:04 -0700

    .

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
 
 export default class Palindrome extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            userInput: '',
+            palindrome: ''
+        }
+    }
+
+    handleChange(val) {
+        this.setState({ userInput: val })
+    }
+
+    //split() divides string into ordered list of substrings -> puts in array according to pattern parameter ///& returns array. Empty string param splits each char.
+    //reverse() reverses an array
+
+    palindromeCheck(userInput) {
+        let str_array = userInput.split("");
+        let output = str_array.reverse().join("");
+        if (userInput === output) {
+            this.setState({ palindrome: 'true' })
+        } else {
+            this.setState({ palindrome: 'false' })
+        }
+    }
+
+
     render() {
         return (
-            <p> Palindrome Component </p>
+            <div className="puzzleBox palindromePB">
+                <h4>Palindrome</h4>
+                <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)} ></input>
+                <button className="confirmationButton" onClick={() => this.palindromeCheck(this.state.userInput)}>Check</button>
+                <span className="resultsBox">Palindrome: {this.state.palindrome}</span>
+            </div >
         )
     }
 }
